0db6a3515efb072f5f61b9760f661dd86c7e5e0f
WreWolf <wrewolf@gmail.com>
2017-11-08 12:30:24 +0300

    Update examples

--- examples/01-demo.php
+++ examples/01-demo.php
@@ -4,49 +4,22 @@
   
   include_once("vendor/autoload.php");
   
-  $cadmusApi = new Client("01904164d2e2db20400612e9d70d24b3dc2db17567ec92cb93c51b87697793c1");  // Ключ к api
+  $api = new Client("01904164d2e2db20400612e9d70d24b3dc2db17567ec92cb93c51b87697793c1");  // Ключ к api
   
-  try
+  $response = $api->call("pg/demo", [
+    "get_a_file" => 0,
+    "assoc"      => "array",
+    "of"         => "parameters",
+  ]);
+  
+  if ($response !== false)
   {
-    $call = $cadmusApi->post("pg/demo", [
-      "get_a_file" => 0,
-      "assoc"      => "array",
-      "of"         => "parameters",
-    ]);
-    
-    $response = $call->getResponse();
-    
-    if (isset($response['message']))
-    {
-      echo "Post Result: " . $response['message'] . "\n";
-    }
-    else
-    {
-      echo "Post Error: " . $response['error'] . "\n";
-    }
-    
-    $call = $cadmusApi->get("pg/demo", [
-      "get_a_file" => 0,
-      "assoc"      => "array",
-      "of"         => "parameters",
-    ]);
-    
-    $response = $call->getResponse();
-    
-    if (isset($response['message']))
-    {
-      echo "Get Result: " . $response['message'] . "\n";
-    }
-    else
-    {
-      echo "Get Error: " . $response['error'] . "\n";
-    }
+    echo "Post Result: " . print_r($response, true) . "\n";
   }
-  catch (Exception $e)
+  else
   {
-    echo json_encode([
-        'status'  => 500,
-        'message' => $e->getMessage(),
-      ],
-        JSON_PRETTY_PRINT) . "\n";
+    echo "Post Error: " . $api->getError() . "\n";
   }
+    
+  
+  
--- examples/02-qr-decoder.php
+++ examples/02-qr-decoder.php
@@ -1,16 +1,11 @@
 <?php
   
-  use Buzz\Message\Form\FormUpload;
   use Cadmus\API\Client;
+  use Cadmus\API\File;
   
   include_once("vendor/autoload.php");
   
   $cadmusApi = new Client("01904164d2e2db20400612e9d70d24b3dc2db17567ec92cb93c51b87697793c1");  // Ключ к api
   
-  $imageFilePath = 'qr.png'; // Путь к картинке
-  $image = new FormUpload($imageFilePath); // Создаём объект файла для загрузки
-  
-  $response = $cadmusApi->post("pg/qr.decode", ['image' => $image]); // Загружаем файл в апи
-  
-  var_dump($response->getResponse());
-  
+  // Имя АПИ, Параметры
+  var_dump($cadmusApi->call("pg/qr.decode", ['image' => new File('qr.png')]));
