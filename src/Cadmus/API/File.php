6ccc919bd5cb3de51c2f9ce5c1497c9d14b55aa6
WreWolf <wrewolf@gmail.com>
2017-11-08 11:51:51 +0300

    File object

---
+++ src/Cadmus/API/File.php
@@ -0,0 +1,17 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * User: wrewolf
+   * Date: 08.11.17
+   * Time: 10:47
+   */
+  
+  namespace Cadmus\API;
+  
+  
+  use Buzz\Message\Form\FormUpload;
+
+  class File extends FormUpload
+  {
+    
+  }
